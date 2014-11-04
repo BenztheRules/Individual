628fb1d7df75a21a2a851c79e8d3c3e900814be7
BenztheRules <benzc75@uwosh.edu>
2014-11-04 10:53:04 -0600

    Initial files
    These are the original files for the project

---
+++ Indie/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.ui.dropdown').dropdown();
+	
+	// $('.dog.shape')
+		// .eq(0)
+			// .shape('flip over')
+			// .end()
+		// .eq(1)
+			// .shape('flip back')
+	// ;
+});
+
